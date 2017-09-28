e7304d5061a760deb920dc1d61f0cfbe0ceb0822
Andreas Remar <andreas.remar@gmail.com>
2017-09-28 14:46:37 +0200

    Implement Init

--- src/Cats.cs
+++ src/Cats.cs
@@ -1,12 +1,24 @@
 using System;
 using SDL2;
 
-namespace cats
-{
-	public class Cats
-	{
-		public Cats ()
-		{
+namespace cats {
+	public class Cats {
+		private IntPtr window = IntPtr.Zero;
+		private IntPtr renderer = IntPtr.Zero;
+
+		public Cats () {
+		}
+
+		public void Init(int width, int height) {
+			window = SDL.SDL_CreateWindow ("Cats#",
+			                               SDL.SDL_WINDOWPOS_UNDEFINED,
+			                               SDL.SDL_WINDOWPOS_UNDEFINED,
+			                               width,
+			                               height,
+			                               SDL.SDL_WindowFlags.SDL_WINDOW_SHOWN);
+			renderer = SDL.SDL_CreateRenderer(window, -1,
+			                                  SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED);
+			SDL.SDL_SetRenderDrawColor (renderer, 0x00, 0x00, 0x00, 0xFF);
 		}
 	}
 }
