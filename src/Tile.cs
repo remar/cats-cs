7b1f77ead887b246b46f08c70eb799487ee8800b
Andreas Remar <andreas.remar@gmail.com>
2017-10-05 12:20:34 +0200

    No need to create two destination rects.

--- src/Tile.cs
+++ src/Tile.cs
@@ -6,19 +6,19 @@ namespace cats {
 		IntPtr image = IntPtr.Zero;
 		SDL.SDL_Rect src = new SDL.SDL_Rect();
 		SDL.SDL_Rect dest = new SDL.SDL_Rect();
-		SDL.SDL_Rect offsetDest = new SDL.SDL_Rect();
+		int xPosition, yPosition;
 
 		public Tile (int x, int y, int tileWidth, int tileHeight) {
-			dest.x = x * tileWidth;
-			dest.y = y * tileHeight;
-			offsetDest.w = dest.w = tileWidth;
-			offsetDest.h = dest.h = tileHeight;
+			xPosition = x * tileWidth;
+			yPosition = y * tileHeight;
+			dest.w = tileWidth;
+			dest.h = tileHeight;
 		}
 
 		public void Draw(IntPtr renderer, int xOffset, int yOffset) {
-			offsetDest.x = dest.x + xOffset;
-			offsetDest.y = dest.y + yOffset;
-			SDL.SDL_RenderCopy (renderer, image, ref src, ref offsetDest);
+			dest.x = xPosition + xOffset;
+			dest.y = yPosition + yOffset;
+			SDL.SDL_RenderCopy (renderer, image, ref src, ref dest);
 		}
 
 		public void Set(TileSource source) {
