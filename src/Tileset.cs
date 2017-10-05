8252d8ac1daaf58181989e8b382cfbc0a971ab9d
Andreas Remar <andreas.remar@gmail.com>
2017-10-05 10:45:38 +0200

    Cache tile sources.

--- src/Tileset.cs
+++ src/Tileset.cs
@@ -8,6 +8,7 @@ namespace cats {
 		private IntPtr image = IntPtr.Zero;
 		private int width;
 		private int height;
+		private TileSource[,] sources;
 
 		public Tileset(string file) {
 			JObject obj = JObject.Parse (File.ReadAllText (file));
@@ -16,10 +17,23 @@ namespace cats {
 			height = (int)imageDef ["height"];
 			string basePath = Util.GetBasePath (file);
 			image = ImageCache.Instance.GetImage (basePath + (string)imageDef ["path"]);
+			SetupSources ();
 		}
 
 		public TileSource GetTileSource (int tileX, int tileY) {
-			return new TileSource (image, tileX * width, tileY * height, width, height);
+			if (sources [tileX, tileY] == null) {
+				sources [tileX, tileY] = new TileSource (image, tileX * width, tileY * height, width, height);
+			}
+			return sources[tileX, tileY];
+		}
+
+		private void SetupSources() {
+			uint format;
+			int access;
+			int textureWidth, textureHeight;
+			SDL.SDL_QueryTexture (image, out format, out access, out textureWidth, out textureHeight);
+			sources = new TileSource[(int)Math.Ceiling(((double)textureWidth) / width),
+			                         (int)Math.Ceiling(((double)textureHeight) / height)];
 		}
 	}
 }
