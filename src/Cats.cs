4881954837c6e6e2f71bab04bbbae8f5ef1e1454
Andreas Remar <andreas.remar@gmail.com>
2017-10-18 14:02:06 +0200

    Make Cats# a static class

--- src/Cats.cs
+++ src/Cats.cs
@@ -3,19 +3,16 @@ using System.Collections.Generic;
 using SDL2;
 
 namespace cats {
-	public class Cats {
-		private IntPtr window = IntPtr.Zero;
-		private IntPtr renderer = IntPtr.Zero;
-		private Dictionary<string,SpriteDefinition> spriteDefinitions = new Dictionary<string,SpriteDefinition>();
-		private Dictionary<int,SpriteInstance> spriteInstances = new Dictionary<int,SpriteInstance>();
-		private TileLayer tileLayer = null;
-		private Dictionary<string,Tileset> tilesets = new Dictionary<string,Tileset> ();
-		private int nextSpriteId = 0;
+	public static class Cats {
+        private static IntPtr window = IntPtr.Zero;
+        private static IntPtr renderer = IntPtr.Zero;
+        private static Dictionary<string,SpriteDefinition> spriteDefinitions = new Dictionary<string,SpriteDefinition>();
+        private static Dictionary<int,SpriteInstance> spriteInstances = new Dictionary<int,SpriteInstance>();
+        private static TileLayer tileLayer = null;
+        private static Dictionary<string,Tileset> tilesets = new Dictionary<string,Tileset> ();
+        private static int nextSpriteId = 0;
 
-		public Cats () {
-		}
-
-		public void Init(int width, int height) {
+		public static void Init(int width, int height) {
 			window = SDL.SDL_CreateWindow ("Cats#",
 			                               SDL.SDL_WINDOWPOS_UNDEFINED,
 			                               SDL.SDL_WINDOWPOS_UNDEFINED,
@@ -29,7 +26,7 @@ namespace cats {
 			SDL.SDL_SetRenderDrawColor (renderer, 0x00, 0x00, 0x00, 0xFF);
 		}
 
-		public void Redraw(float delta) {
+		public static void Redraw(float delta) {
 			int deltaMillis = (int)(delta * 1000);
 			SDL.SDL_RenderClear (renderer);
 			if (tileLayer != null) {
@@ -41,52 +38,52 @@ namespace cats {
 			SDL.SDL_RenderPresent (renderer);
 		}
 
-		public void SetBackgroundColor(byte red, byte green, byte blue) {
+		public static void SetBackgroundColor(byte red, byte green, byte blue) {
 			SDL.SDL_SetRenderDrawColor (renderer, red, green, blue, 0xFF);
 		}
 
-		public void LoadSprite(string file) {
+		public static void LoadSprite(string file) {
 			string name = Util.FilenameToName (file);
 			spriteDefinitions.Add (name, new SpriteDefinition (file));
 		}
 
-		public int CreateSpriteInstance(string spritename) {
+		public static int CreateSpriteInstance(string spritename) {
 			int spriteId = nextSpriteId;
 			nextSpriteId++;
 			spriteInstances.Add (spriteId, new SpriteInstance (spriteDefinitions [spritename]));
 			return spriteId;
 		}
 
-		public void RemoveSpriteInstance(int spriteId) {
+		public static void RemoveSpriteInstance(int spriteId) {
 			spriteInstances.Remove (spriteId);
 		}
 
-		public void ShowSprite(int spriteId, bool visible) {
+		public static void ShowSprite(int spriteId, bool visible) {
 			spriteInstances [spriteId].Visible = visible;
 		}
 
-		public void SetSpritePosition(int spriteId, int x, int y) {
+        public static void SetSpritePosition(int spriteId, int x, int y) {
 			spriteInstances [spriteId].SetPosition (x, y);
 		}
 
-		public void SetAnimation(int spriteId, string animation) {
+        public static void SetAnimation(int spriteId, string animation) {
 			spriteInstances [spriteId].SetAnimation (animation);
 		}
 
-		public void SetupTileLayer(int width, int height, int tileWidth, int tileHeight) {
+        public static void SetupTileLayer(int width, int height, int tileWidth, int tileHeight) {
 			tileLayer = new TileLayer (width, height, tileWidth, tileHeight);
 		}
 
-		public void LoadTileset(string file) {
+        public static void LoadTileset(string file) {
 			string name = Util.FilenameToName (file);
 			tilesets.Add (name, new Tileset (file));
 		}
 
-		public void SetTile(int x, int y, string tileset, int tileX, int tileY) {
+        public static void SetTile(int x, int y, string tileset, int tileX, int tileY) {
 			tileLayer.SetTile (x, y, tilesets[tileset].GetTileSource(tileX, tileY));
 		}
 
-		public void SetScroll(int x, int y) {
+        public static void SetScroll(int x, int y) {
 			tileLayer.SetScroll (x, y);
 		}
 	}
